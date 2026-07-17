e28aaf02c2395b236817c7206d6b9af1e341123a
Reyette Roxylious Atelier <87901027+Zeev-x@users.noreply.github.com>
2026-07-17 12:14:44 +0700

    Update wifi.py

--- wifi.py
+++ wifi.py
@@ -5,8 +5,6 @@ import time
 wlan = network.WLAN(network.STA_IF)
 
 def connect(ssid, password, ip=None, gateway=None, subnet=None, dns=None):
-    """Koneksi ke WiFi dengan opsi IP statis (fallback DHCP)"""
-    # Reset interface dulu biar state bersih
     wlan.active(False)
     time.sleep(0.5)
     wlan.active(True)
@@ -16,7 +14,7 @@ def connect(ssid, password, ip=None, gateway=None, subnet=None, dns=None):
 
     timeout = 20
     while not wlan.isconnected() and timeout > 0:
-        print("Waiting WiFi...")
+        #print("Waiting WiFi...")
         time.sleep(1)
         timeout -= 1
 
@@ -26,7 +24,6 @@ def connect(ssid, password, ip=None, gateway=None, subnet=None, dns=None):
                 wlan.ifconfig((ip, subnet, gateway, dns))
             except Exception as e:
                 print("Static IP failed:", e)
-                # fallback ke DHCP
         print("WiFi Connected")
         print("IP:", wlan.ifconfig()[0])
         return True
@@ -35,14 +32,11 @@ def connect(ssid, password, ip=None, gateway=None, subnet=None, dns=None):
     return False
 
 def ip():
-    """Ambil IP address saat ini"""
     return wlan.ifconfig()[0] if wlan.isconnected() else None
 
 def status():
-    """Cek status koneksi"""
     return wlan.isconnected()
 
 def disconnect():
-    """Putuskan koneksi WiFi"""
     wlan.disconnect()
     wlan.active(False)
